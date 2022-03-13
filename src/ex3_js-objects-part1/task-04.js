02ee4fa3cb4ad37ab80c8509ba8031c6022f45a8
Sergey Volodin <100468291+volodinSergey@users.noreply.github.com>
2022-03-14 00:04:17 +0300

    Fix task-04.js
    1. Убрал проверку на for in и оставил только hasOwnProperty ,потому что мы работаем в маленьком локальной задаче на проверку свойтсва объекта и не лезем в прототипы и их цепочки, а следовательно нам достаточно проверить наличие именно собственного свойства
    
    2. Сделал в условии перенос на несколько строк для лучшей читаемости

--- src/ex3_js-objects-part1/task-04.js
+++ src/ex3_js-objects-part1/task-04.js
@@ -1,7 +1,9 @@
 const compareObjectPropertyWithStringAndAddNewProperty = (string, originalObject) => {
   const newObject = { ...originalObject };
 
-  if (!(string in newObject) && !(newObject.hasOwnProperty(string))) newObject[`${string}`] = 'new';
+   if (!(newObject.hasOwnProperty(string))) {
+    newObject[`${string}`] = 'new';
+  }
 
   return newObject;
 };
