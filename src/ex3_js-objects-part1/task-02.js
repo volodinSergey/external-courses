47c0007863c272ca741b4496eee6d3c5161fe86a
Sergey Volodin <100468291+volodinSergey@users.noreply.github.com>
2022-03-13 23:45:20 +0300

    Fix task-02.js
    Избавился от объявления лишней переменной.Задачу можно решить без этого объявления

--- src/ex3_js-objects-part1/task-02.js
+++ src/ex3_js-objects-part1/task-02.js
@@ -1,7 +1,5 @@
 const showObjectKeysAndValuesInConsole = (object) => {
-  const transformedObjectInArrayWithKeysAndEntries = Object.entries(object);
-
-  transformedObjectInArrayWithKeysAndEntries.forEach(([key, value]) => console.log(`${key}: ${value}`));
+  Object.entries(object).forEach(([key, value]) => console.log(`${key}: ${value}`));
 
   return undefined;
 };
