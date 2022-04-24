e265991a640987d3593ae1e2b57059192a63f6bb
Volodin Sergey <volodin7ergey@icloud.com>
2022-04-24 23:58:25 +0300

    Added script

---
+++ src/ex11_js_dom/script.js
@@ -0,0 +1,79 @@
+const sliderConfig = {
+  slidesParent: document.querySelector('.slider__slides'),
+  slidesClass: 'slider__slides-img',
+  buttonsParent: document.querySelector('.slider__buttons'),
+  prevButton: document.querySelector('.slider__buttons-button_prev'),
+  nextButton: document.querySelector('.slider__buttons-button_next'),
+  activeClass: 'activeSlide',
+};
+
+const slidesDataBase = [
+  'images/slides/slide1.jpg',
+  'images/slides/slide2.jpg',
+  'images/slides/slide3.jpg',
+  'images/slides/slide4.jpg',
+  'images/slides/slide5.jpg',
+  'images/slides/slide6.jpg',
+];
+
+const slider = (config) => {
+  const {
+    slidesParent,
+    buttonsParent,
+    prevButton,
+    nextButton,
+    slidesClass,
+    activeClass,
+  } = config;
+
+  let slideIndex = 0;
+
+  const renderSlides = () => {
+    slidesDataBase.forEach((slideSrc) => {
+      const slide = document.createElement('img');
+
+      slide.src = slideSrc;
+      slide.alt = 'slide';
+      slide.classList.add(slidesClass);
+
+      slidesParent.append(slide);
+    });
+
+    return document.querySelectorAll(`.${slidesClass}`);
+  };
+
+  const slides = renderSlides();
+
+  const showSlide = () => {
+    slides.forEach((slide) => slide.classList.remove(activeClass));
+    slides[slideIndex].classList.add(activeClass);
+  };
+
+  showSlide();
+
+  const moveSlidesBackward = () => {
+    slideIndex -= 1;
+
+    if (slideIndex < 0) slideIndex = slides.length - 1;
+
+    showSlide(slideIndex);
+  };
+
+  const moveSlidesForward = () => {
+    slideIndex += 1;
+
+    if (slideIndex > slides.length - 1) slideIndex = 0;
+
+    showSlide(slideIndex);
+  };
+
+  buttonsParent.addEventListener('click', (e) => {
+    const target = e.target;
+
+    if (target && target === prevButton) moveSlidesBackward();
+
+    if (target && target === nextButton) moveSlidesForward();
+  });
+};
+
+slider(sliderConfig);
